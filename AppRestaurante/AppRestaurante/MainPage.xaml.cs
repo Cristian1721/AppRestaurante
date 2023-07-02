2172a3b3dd1bee3b3b49f057857e5958d3ec2854
Cristian Caiza <Cristian Caiza@DESKTOP-QLGSBDF>
2023-07-02 09:32:36 -0500

    Agregar archivos de proyecto.

---
+++ AppRestaurante/AppRestaurante/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppRestaurante
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
